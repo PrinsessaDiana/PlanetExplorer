eeb3134fdb7ed3775bb9ae8a42ba169708ab320e
somename <someemail>
2015-10-23 11:48:05 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -3,7 +3,14 @@ package org.unioulu.tol.sqat2015.planetExplorer;
 // Before submitting write your ID and finish time here. Your ID is written on project description sheets.
 // ID:
 // Finish time:
+
+
+
+
 public class PlanetExplorer {
+	
+	int [][] planet = new int [6][6];
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -12,16 +19,13 @@ public class PlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
 	 */
 		
-		//Planeetta m‰‰ritell‰‰n
 		
+	
 		
 	}
 	
-	public void Planet(int x, int y) {
-		
-		
-		
-	}
+	
+	
 	
 	public String executeCommand(String command){
 		
